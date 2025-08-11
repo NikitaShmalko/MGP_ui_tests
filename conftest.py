f5d503e7eaf049a43fcd0b74f81531bc06dd2541
Nikita Shmalko <shmalkopress@gmail.com>
2025-08-11 14:15:23 +0300

    added workflow

--- conftest.py
+++ conftest.py
@@ -12,16 +12,11 @@ def driver():
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-gpu")
 
-    # уникальная папка профиля на каждый тест
-    user_data_dir = tempfile.mkdtemp(prefix="chrome-profile-")
-    options.add_argument(f"--user-data-dir={user_data_dir}")
-
     driver = webdriver.Chrome(options=options)
     try:
         yield driver
     finally:
         driver.quit()
-        shutil.rmtree(user_data_dir, ignore_errors=True)
 
 @pytest.fixture
 def cart(driver):
